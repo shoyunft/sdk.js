fcc1c9216b78b8691cb094ac0d86d6e53ac0ee17
Team LevX <team@levx.io>
2021-09-28 01:36:58 +0900

    Fix

--- src/strategies/DutchAuction.ts
+++ src/strategies/DutchAuction.ts
@@ -3,23 +3,23 @@ import { BigNumber, BigNumberish, BytesLike, utils } from "ethers";
 class DutchAuction {
     static from(params: BytesLike) {
         const result = utils.defaultAbiCoder.decode(["uint256", "uint256", "uint256"], params);
-        return new DutchAuction(result.startPrice, result.endPrice, result.startBlock);
+        return new DutchAuction(result.startPrice, result.endPrice, result.startedAt);
     }
 
     startPrice: BigNumber;
     endPrice: BigNumber;
-    startBlock: BigNumber;
+    startedAt: BigNumber;
 
-    constructor(startPrice: BigNumberish, endPrice: BigNumberish, startBlock: BigNumberish) {
+    constructor(startPrice: BigNumberish, endPrice: BigNumberish, startedAt: BigNumberish) {
         this.startPrice = BigNumber.from(startPrice.toString());
         this.endPrice = BigNumber.from(endPrice.toString());
-        this.startBlock = BigNumber.from(startBlock.toString());
+        this.startedAt = BigNumber.from(startedAt.toString());
     }
 
     encode() {
         return utils.defaultAbiCoder.encode(
             ["uint256", "uint256", "uint256"],
-            [this.startPrice, this.endPrice, this.startBlock]
+            [this.startPrice, this.endPrice, this.startedAt]
         );
     }
 }
